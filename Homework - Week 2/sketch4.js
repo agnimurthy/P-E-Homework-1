1737eed68daff2ccfa808fef04e86cdc1b291066
agnimurthy <alakhe-murthy@cca.edu>
2017-01-28 22:56:50 -0800

    Create sketch4.js

---
+++ Homework - Week 2/sketch4.js
@@ -0,0 +1,29 @@
+var x = 100;
+var y = 100;
+var move = 7.5;
+
+
+function setup() {
+  createCanvas(600, 600);
+  background (25);
+
+}
+
+function draw() {
+  
+  background(255);
+
+  fill (255);
+  ellipse (x, y, 50, 50);
+  
+  x = x + move;
+  	
+  	 if (x > 575 || x < 25)  {
+     	move = -move;
+  	}
+}
+
+function mousePressed() {
+      x = mouseX;
+      y = mouseY;
+  }
