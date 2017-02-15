8acaa526c2032725e73a25381087a75bbe3ec4af
agnimurthy <alakhe-murthy@cca.edu>
2017-02-15 08:52:19 -0800

    Create sketch5.js

---
+++ Homework - Week 4/sketch5.js
@@ -0,0 +1,18 @@
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+function draw() { 
+  background(0);
+  noStroke();
+	colorMode(HSB);
+  
+  var h = 0;
+  fill(255);
+  for(var posy = 20; posy <= 200; posy += 20){
+  	for(var posx = posy; posx <= 400-posy; posx += 20){
+  		ellipse(posx,posy,15,15);
+  		h = h +0.65;
+	  }
+  }
+}
