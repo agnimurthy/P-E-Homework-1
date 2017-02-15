58b2a28e5a7e7c188391bd8aa8fee0b06b291aa8
agnimurthy <alakhe-murthy@cca.edu>
2017-02-15 08:40:01 -0800

    Update sketch4.js

--- Homework - Week 4/sketch4.js
+++ Homework - Week 4/sketch4.js
@@ -6,10 +6,14 @@ function draw() {
   background(220);
   noStroke();
 	colorMode(HSB);
-  for(var posx = 0; posx <= 360; posx += 20){
+  
+  var h = 0;
+  
   for(var posy = 0; posy <= 360; posy += 20){
-  fill(posy + (posx/20),100,100)
-  ellipse(20 + posx,20 + posy,15,15);
-  }
+  	for(var posx = 0; posx <= 360; posx += 20){
+  		fill(h,100,100)
+  		ellipse(20 + posx,20 + posy,15,15);
+  		h = h +0.65;
+	  }
   }
 }
