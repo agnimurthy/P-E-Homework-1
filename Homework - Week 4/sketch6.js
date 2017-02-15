5b80fe7e00c00f61170aa87c7198d0f3576cdea5
agnimurthy <alakhe-murthy@cca.edu>
2017-02-15 09:27:46 -0800

    Update sketch6.js

--- Homework - Week 4/sketch6.js
+++ Homework - Week 4/sketch6.js
@@ -5,14 +5,11 @@ function setup() {
 function draw() { 
   background(0);
   noStroke();
-	colorMode(HSB);
-  
-  var h = 0;
+	
   fill(255);
   for(var posy = 20; posy <= 200; posy += 20){
   	for(var posx = posy; posx <= 400-posy; posx += 20){
   		ellipse(posx,posy,15,15);
-  		h = h +0.65;
 	  }
   }
   
