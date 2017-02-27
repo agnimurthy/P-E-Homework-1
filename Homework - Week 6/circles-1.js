93e8f18a7e5f103d8db51230d41bc28ba14c75c8
agnimurthy <alakhe-murthy@cca.edu>
2017-02-27 12:50:14 -0800

    Update circles-1.js

--- Homework - Week 6/circles-1.js
+++ Homework - Week 6/circles-1.js
@@ -1,12 +1,17 @@
-var sizes = [40, 70, 90, 30, 60, 21, 32, 69, 47, 93];
+var sizes = [40, 70, 90, 30, 60];
 
 function setup() {
-  createCanvas(1000, 1000);
+  createCanvas(1025, 500);
 background(255);
   noStroke();
+  append(sizes, 21);
+  append(sizes, 32);
+  append(sizes, 69);
+  append(sizes, 47);
+  append(sizes, 93);
   for (var i = 0; i < sizes.length; i += 1) {
     fill(sizes[i]);
     ellipse(50+i*100, height/2, sizes[i], sizes[i]);
   }
+  
 }
-
