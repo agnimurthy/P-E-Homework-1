e69b1258ebfc9b6778223a88a1a39d0f9213904e
agnimurthy <alakhe-murthy@cca.edu>
2017-02-27 07:27:42 -0800

    Create circles-2.js

---
+++ Homework - Week 6/circles-2.js
@@ -0,0 +1,13 @@
+var sizes = [40, 70, 90, 30, 60, 21, 32, 69, 47, 93];
+
+function setup() {
+  createCanvas(1000, 1000);
+background(255);
+  noStroke();
+  colorMode(HSB);
+  for (var i = 0; i < sizes.length; i += 1) {
+    fill(sizes[i],200,200);
+    ellipse(50+i*100, height/2, sizes[i], sizes[i]);
+  }
+}
+
